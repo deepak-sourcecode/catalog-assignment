50cc42981afc68786bb162d67ee5c2340e0fbc2b
deepak-sourcecode <deepak.sourcecode@gmail.com>
2024-09-18 16:48:23 +0530

    feat(rtk-query): add test pokemon api service

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { pokemonApi } from '../services/pokemonApi';
 import rootReducer from './rootReducer';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
