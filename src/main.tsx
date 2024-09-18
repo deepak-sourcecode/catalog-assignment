39c0236eab144cc0990e993401fcf3124443e2c5
deepak-sourcecode <deepak.sourcecode@gmail.com>
2024-09-18 15:56:26 +0530

    refactor: remove app.tsx and migrate to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,36 @@
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-import App from './App.tsx';
+import Dashboard from './routes/Dashboard/index.tsx';
 
 import './index.css';
 
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        fontFamily: 'Circular Std',
+      },
+    }),
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <Dashboard />,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider theme={theme}>
+      <main className="h-screen w-screen">
+        <RouterProvider router={router} />
+      </main>
     </ChakraProvider>
   </StrictMode>,
-)
+);
