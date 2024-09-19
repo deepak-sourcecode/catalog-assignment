c7382132a8134c7ef4c3abdf035a142fe02fc9ff
deepak-sourcecode <deepak.sourcecode@gmail.com>
2024-09-19 10:27:27 +0530

    refactor: update coin price metrics for api data

--- src/components/Dashboard/CoinPriceMetrics.tsx
+++ src/components/Dashboard/CoinPriceMetrics.tsx
@@ -1,15 +1,80 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 
+import { SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+
+import { useAppSelector } from '../../hooks';
+import { useGetCoinMarketDataByTimeRangeQuery } from '../../services/dashboardApi';
+import { selectActiveCoinId, selectStaticTimePeriodFilterOption } from '../../slices/dashboardSlice';
 import { twclsx } from '../../utils';
 
 const CoinPriceMetrics: FunctionComponent = () => {
+  const activeCoinId = useAppSelector(selectActiveCoinId);
+  const { duration, unit } = useAppSelector(selectStaticTimePeriodFilterOption);
+  const { data, isFetching } = useGetCoinMarketDataByTimeRangeQuery(
+    {
+      id: activeCoinId,
+      vs_currency: 'usd',
+      duration,
+      unit,
+    },
+    { skip: !activeCoinId || !duration || !unit },
+  );
+
+  const [formattedPrice, setFormattedPrice] = useState<string>('N.A');
+  const [trend, setTrend] = useState<string>('N.A');
+  const [formattedDifference, setFormattedDifference] = useState<string>('');
+  const [formattedDifferencePercent, setFormattedDifferencePercent] = useState<string>('');
+
+  useEffect(() => {
+    if (data?.prices?.length) {
+      const totalPoints = data?.prices?.length;
+      const initialPrice = data?.prices?.[0]?.[1];
+      const latestPrice = data?.prices?.[totalPoints - 1]?.[1];
+
+      const priceDifference = latestPrice - initialPrice;
+      const priceDifferencePercent = (priceDifference / initialPrice) * 100;
+      const trend = priceDifference >= 0 ? '+' : '-';
+
+      const formattedPrice = latestPrice?.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      const formattedDifference = Math.abs(priceDifference)?.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      const formattedDifferencePercent = Math.abs(priceDifferencePercent)?.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      setTrend(trend);
+      setFormattedPrice(formattedPrice);
+      setFormattedDifference(formattedDifference);
+      setFormattedDifferencePercent(formattedDifferencePercent);
+    }
+  }, [data]);
+
+  if (isFetching) {
+    return (
+      <div className={twclsx('flex flex-col gap-1', 'md:gap-3')}>
+        <SkeletonCircle size="10" />
+        <SkeletonText noOfLines={3} spacing="4" skeletonHeight="2" />
+      </div>
+    );
+  }
+
   return (
     <div className={twclsx('flex flex-col gap-1', 'md:gap-3')}>
       <div className={twclsx('flex flex-wrap', 'md:flex-nowrap')}>
-        <h1 className="text-dark-1 text-7xl">63,179.71</h1>
-        <h2 className="text-gray-1 ps-1.5 pt-1.5 text-2xl">USD</h2>
+        <h1 className="text-7xl text-dark-1">{formattedPrice}</h1>
+        <h2 className="ps-1.5 pt-1.5 text-2xl text-gray-1">USD</h2>
       </div>
-      <span className="text-green-1 text-lg">+ 2,161.42 (3.54%)</span>
+      <span
+        className={`${trend === '-' ? 'text-red-500' : 'text-green-1'} text-lg`}
+      >{`${trend} ${formattedDifference} (${formattedDifferencePercent}%)`}</span>
     </div>
   );
 };
